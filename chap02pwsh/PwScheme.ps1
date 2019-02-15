1bbc963f192014f475106fabbcbb916f158b599c
naens <naens@gmx.com>
2019-02-15 17:49:44 +0100

    chap02pwsh: Evaluate expressions with the Evaluator

--- chap02pwsh/PwScheme.ps1
+++ chap02pwsh/PwScheme.ps1
@@ -1,14 +1,24 @@
 # PowerScheme, Scheme interpreter written in PowerShell
 
-$Path = $args[0]
-$Text = [System.IO.File]::ReadAllText( ( Resolve-Path $Path ) )
-
 . ".\Tokenizer.ps1"
-$Tokens = Get-Tokens($Text)
-$Tokens | ForEach-Object {Write-Host $_}
-
 . ".\Parser.ps1"
-$Exps = Parse-Tokens($Tokens)
+. ".\Evaluator.ps1"
+. ".\Environment.ps1"
+
+$Path = $args[0]
+
+$Text = [System.IO.File]::ReadAllText( (Resolve-Path $Path) )
+
+$Tokens = Get-Tokens $Text
+#$Tokens | ForEach-Object {Write-Host $_}
+
+$Exps = Parse-Tokens $Tokens
+#$Exps | ForEach-Object { Write-Host (Exp-To-String $_) }
+
+$env = New-Object Environment
+$denv = New-Object Environment
 $Exps | ForEach-Object {
     Write-Host (Exp-To-String $_)
+    $exp = Evaluate $_
+    Write-Host EXPRESSION: (Exp-To-String $exp $env $denv)
 }
