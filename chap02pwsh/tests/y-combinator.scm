1e856e1a72102b8e597843dc4b31d52505f99f9a
naens <naens@gmx.com>
2019-03-04 21:25:05 +0100

    chap02pwsh: Y-combinator test: pass

---
+++ chap02pwsh/tests/y-combinator.scm
@@ -0,0 +1,17 @@
+(define fix
+ (let ((d (lambda (w)
+           (lambda (f)
+            (f (lambda (x) (((w w) f) x)))))))
+  (d d)))
+
+(define (meta-fact f)
+ (lambda (n)
+  (if (= n 0)
+   1
+   (* n (f (- n 1))))))
+
+((fix meta-fact) 3)
+; 6
+
+((fix meta-fact) 10)
+; 3628800
