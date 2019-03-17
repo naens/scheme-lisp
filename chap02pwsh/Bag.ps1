0f45ba4b2d04e722325278cb7f46e6838f38b802
naens <naens@gmx.com>
2019-03-17 11:38:47 +0100

    chap02pwsh: fix bag quotes when in REPL

--- chap02pwsh/Bag.ps1
+++ chap02pwsh/Bag.ps1
@@ -4,14 +4,23 @@ class Bag {
     [System.Collections.ArrayList]$tokens
     $level
 
+    $is_quote
     Bag() {
         $this.queue = @()
         $this.tokens = @()
         $this.level = 0
+        $this.is_quote = $false
     }
 
     [void] addTokens($list) {
         foreach ($token in $list) {
+            if ($token.type -eq "Quote") {
+                if ($this.is_quote -or $this.tokens.count -eq 0) {
+                    $this.is_quote = $true
+                }
+            } else {
+                $this.is_quote = $false
+            }
             $this.tokens.Add($token)
             if ($token.type -eq "ParOpen") {
                 $this.level++
@@ -19,7 +28,7 @@ class Bag {
             if ($token.type -eq "ParClose") {
                 $this.level--
             }
-            if ($this.level -eq 0) {
+            if ($this.level -eq 0 -and -not $this.is_quote) {
                 $this.queue.Add($this.tokens)
                 $this.tokens = @()
             } elseif ($this.level -lt 0) {
